22f651c754de185524a0c180d863a4498f98dc53
Mattia Baertschi <114213434+MattiaBaertschi@users.noreply.github.com>
2022-10-04 11:27:17 +0200

    a

---
+++ Uebung02/Aufgabe01.js
@@ -0,0 +1,9 @@
+var L = [];
+
+for (var i=1; i<100; i++) {
+    if (i % 2 != 0) {
+        L.push(i);
+    };
+}
+
+console.log(L);
---
+++ Uebung02/Aufgabe02.js
@@ -0,0 +1,6 @@
+function wuerfeln() {
+    return Math.round(5*Math.random()+1);
+}
+
+a = wuerfeln();
+console.log(a);
